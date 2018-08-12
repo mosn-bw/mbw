e46f0ecf89986b77b2b23d0dafb0ca99eafc2a27
PH-13 <31429243+PHIEZUKE@users.noreply.github.com>
2018-08-12 17:05:34 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -30,7 +30,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'CHROME-OS'
+    SYSTEM_NAME = 'PH-13'
     SYSTEM_VER  = '1'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
