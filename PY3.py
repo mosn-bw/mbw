fa9828c542ce184b16a42868a3fac9e78cf3239c
PH-13 <31429243+PHIEZUKE@users.noreply.github.com>
2018-09-07 14:06:35 +0700

    Update PY3.py

--- PY3.py
+++ PY3.py
@@ -12,7 +12,7 @@ from googletrans import Translator
 import youtube_dl
 #BOT INI BUAT KALIAN YANG MEMANG MEMBUTUHKAN
 cl = LineClient()
-#cl = LineClient(authToken='Ev49wbWDciJuzQi8UG6e.mDIRQD+WthuKlYOGL7ITRG.pAFPZhYHHPSFjqOGUkKn9dqHCER/ZNDUDgOBpLrWJe4=')   #PH-13
+#cl = LineClient(authToken='EwuDzAX1HOChhzFJMivb.Sp/s6yipA4kI8lGDz1Jc/W./IDEYRL3fL3zmq9ikK8ECpVJH4UzuQa9BsSXAgUOX+U=')   #PH-13
 cl.log("Auth Token : " + str(cl.authToken))
 channel = LineChannel(cl)
 cl.log("Channel Access Token : " + str(channel.channelAccessToken))
